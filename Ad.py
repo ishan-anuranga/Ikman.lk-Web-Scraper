76b533f413b01d7c57150c797a8ae8834fae5877
Ishan Pathirana <isan0007@gmail.com>
2023-09-27 12:16:53 +0530

    Create Ad class

---
+++ Ad.py
@@ -0,0 +1,13 @@
+class Ad:
+
+    def __init__(self, id, title, link, premium_member, verified_member, location, category, price, top_ad, img_link):
+        self.id = id
+        self.title = title
+        self.link = link
+        self.premium_member = premium_member
+        self.verified_member = verified_member
+        self.location = location
+        self.category = category
+        self.price = price
+        self.top_ad = top_ad
+        self.img_link = img_link
